5c6920a0b51ee6e8c4799c792eff6a6df45312c9
Pavel Gric <pavelgric@gmail.com>
2026-07-09 17:49:39 +0200

    Render triangle

---
+++ src/Application.cpp
@@ -0,0 +1,73 @@
+// #define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+// #include <gl2d/gl2d.h>
+#include <openglErrorReporting.h>
+
+// #include "imgui.h"
+// #include "backends/imgui_impl_glfw.h"
+// #include "backends/imgui_impl_opengl3.h"
+// #include "imguiThemes.h"
+
+static void error_callback(int error, const char *description)
+{
+	std::cout << "Error: " << description << "\n";
+}
+
+int main(void)
+{
+
+	glfwSetErrorCallback(error_callback);
+
+	GLFWwindow *window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#endif
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	// enableReportGlErrors();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw triangle using legacy API
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
